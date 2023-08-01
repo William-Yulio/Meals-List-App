6b7104a5c1597056cb0a97a4b9ead886d2cfe0ad
William-Yulio <william.yulio331@gmail.com>
2023-08-01 15:12:00 +0700

    Initial Commit

---
+++ Meal List App/Meal_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_List_AppApp.swift
+//  Meal List App
+//
+//  Created by William Yulio on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Meal_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
